e5c3f687a8c3f1478d9ad3a32ec72ff7645df72f
wahyu <wahyubudik9@student.uns.ac.id>
2023-03-10 20:41:32 +0700

    wahyu.py

---
+++ Modul_01/wahyu.py
@@ -0,0 +1,11 @@
+nama_depan = "wahyu"
+nama_blk = "budi"
+
+angka = 10
+desimal = 0,5
+boolean = True
+kelas_D = {"nama": "ksl d", "dll"}
+dictionary = {"nama": "wahyu", "kelas": "KLS D"}
+
+print(nama_depan)
+print("nama saya wahyu")
