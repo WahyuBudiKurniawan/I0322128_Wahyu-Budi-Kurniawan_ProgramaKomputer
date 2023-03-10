1909c8f8321fc300c885cb177bc56115fffe25f7
wahyu <wahyubudik9@student.uns.ac.id>
2023-03-10 21:23:06 +0700

    Update wahyu.py

--- Modul_01/wahyu.py
+++ Modul_01/wahyu.py
@@ -1,11 +1,14 @@
-nama_depan = "wahyu"
-nama_blk = "budi"
+nama_depan = "Wahyu"
+nama_belakang = "Budi"
+kelas = "D"
+nim = "128"
 
 angka = 10
 desimal = 0,5
 boolean = True
-kelas_D = {"nama": "ksl d", "dll"}
-dictionary = {"nama": "wahyu", "kelas": "KLS D"}
+kelas_d = ["Wahyu", "Kelas D", "dll"]
+data = {"Nama": "Wahyu", "Kelas": "Kls D","Nim": 128}
 
 print(nama_depan)
-print("nama saya wahyu")
+print("nama saya adalah Wahyu")
+print(f"nama saya adalah {nama_depan} {nama_belakang} dari kelas {kelas} dengan Nim {nim}")
