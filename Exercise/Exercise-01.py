20023e08256954dc21237d553f23c976bb6535ad
wahyu <wahyubudik9@student.uns.ac.id>
2023-03-11 22:41:02 +0700

    Exercise

---
+++ Exercise/Exercise-01.py
@@ -0,0 +1,28 @@
+#biodata diri
+nama_lengkap = "Wahyu Budi Kurniawan"
+nama_panggilan = "Wahyu"
+tempat_tanggal_lahir = "Purworejo, 19 April 2004"
+universitas = "Universitas Sebelas Maret"
+prodi = "Teknik Industri"
+kelas = "D"
+NIM = "I0322128"
+
+#kondisi geografis kampung halaman
+visualisasi_kampung_halaman = "saya berasal dari sebuah daerah yang tidak terlalu terkenal yakni Kabupaten Purworejo, lebih tepatnya yakni di Desa Sukoharjo Kec. Kutoarjo, Kab. Purworejo, Prov. Jawa Tengah. Di daerah sekitar desa saya tidak terdapat sebuah tempat wisata ataupun monumen yang cukup khas karena desa saya berjarak kurang lebih 2 km dari kota. Namun terdapat suatu bangunan yang dekat dengan desa yakni Stasiun Kutoarjo yang berada kurang lebih 2 km dari desa saya. Selain itu terdapat juga satu terminal yakni Terminal Kutoarjo. Meskipun terkesan tidak mempunyai bangunan yang khas, desa saya masih cukup asri. Banyak sawah yang menghampar luas dengan pemandangan yang cukup bagus, polusi pun sedikit apabila olahraga pagi maka akan terasa sejuknya. Saya cukup bangga di desa saya, bisa dibilang letaknya pun cukup unik yakni udara di desa saya sejuk dan sangat enak apabila digunakan untuk olahraga. Namun selisih 2 km langsung kota yang cukup banyak polusi, ketika saya ke kota untuk olahraga pada pagi hari cuacanya cukup panas. Selain itu semua, desa saya mempunyai makanan khas yang cukup unik yakni bernama clorot. Clorot terbuat dari dari tepung yang dicampur dengan gula jawa kemudian di bungkus dengan menggunakan daun kelapa yang dibiasa dinamakan janur yang berbentuk seperti terompet. Kemudian terdapat sebuah jalan yang cukup unik yang bernama Gunung Tugel (Gunung putus), dinamakan seperti itu karena dahulu jalan tersebut ialah sebuah pegunungan yang banyak hewan liar kemudian diaspal dan dijadikan jalan."
+
+#harapan sebagai lulusan Teknik Industri UNS
+haparan_sebagai_lulusan_teknik_industri = "Harapan saya sebagai lulusan dari Teknik Industri UNS ialah dapat menjadi seorang enggineer yang memahami seluk beluk Teknik Industri serta dapat menerapkan bidang keilmuan yang ada agar nantinya dapat bekerja di perusahaan-perusahaan. Dan apabila saya mempunyai kesempatan untuk melanjutkan S2 maka saya akan mencobanya untuk dapat menambah wawasan serta pengalaman untuk saya"
+
+#output
+print("========================= EXERCISE-01 =========================")
+print(f"Nama lengkap saya adalah {nama_lengkap}")
+print(f"Saya biasanya dipanggil dengan {nama_panggilan}")
+print(f"Tempat tanggal lahir saya adalah {tempat_tanggal_lahir}")
+print(f"Sekarang saya kuliah di {universitas}")
+print(f"Saya mengambil program studi {prodi}")
+print(f"Dan saat ini saya berada di kelas {kelas} dengan NIM {NIM}")
+print("===============================================================")
+print(f"visualisasi kampung saya yaitu {visualisasi_kampung_halaman}")
+print("===============================================================")
+print(f"Saya juga mempunyai harapan setelah lulus nanti. Adapun {haparan_sebagai_lulusan_teknik_industri}")
+print("===============================================================")
